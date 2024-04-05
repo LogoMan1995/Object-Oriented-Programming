e9634626a9635f931d1944f44e2f39554db18daa
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-05 18:18:11 +0400

    Use a Constructor to Create Objects

---
+++ ex06.js
@@ -0,0 +1,13 @@
+// 6. Используйте Dog конструктор из прошлого урока, чтобы создать новый экземпляр Dog, присвоив его переменной hound.
+
+
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+  }
+
+let hound = new Dog();
+console.log(hound);
+
