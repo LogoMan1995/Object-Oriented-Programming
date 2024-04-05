7884ce042e8ac937264a12fb6395baa44e6cfc4d
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-05 17:59:33 +0400

    Define a Constructor Function

---
+++ ex05.js
@@ -0,0 +1,9 @@
+// 5. Создайте конструктор , Dog со свойствами name, color и numLegs для которых заданы значения строки, строки и числа соответственно.
+
+
+
+function Dog(){
+    this.name = "'Hachiko'"
+    this.color = "beige"
+    this.numLegs = 4;
+  }
