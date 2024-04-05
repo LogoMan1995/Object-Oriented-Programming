bb4a02e5a564fc37d9f4ee6e2386e263e7c5d553
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-05 17:26:52 +0400

    Create a Basic JavaScript Object

---
+++ ex01.js
@@ -0,0 +1,9 @@
+// 1. Создайте dog объект со свойствами name и numLegs и задайте для них строку и число соответственно.
+
+
+let dog = {
+    name: 'Hachiko',
+    numLegs: 4
+}
+
+console.log(dog);
