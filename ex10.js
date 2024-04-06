dc2e2475837453d856ff85622286c95b5fc10729
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-06 14:51:36 +0400

    Use Prototype Properties to Reduce Duplicate Code

---
+++ ex10.js
@@ -0,0 +1,15 @@
+// 10. Добавить numLegs прототип в prototype список Dog.
+
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  
+  
+  Dog.prototype.numLegs = 4;
+  let beagle = new Dog("Snoopy");
+  
+
+
+
